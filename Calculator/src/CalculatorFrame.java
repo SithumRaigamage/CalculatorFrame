a7939e424d9d5b88accf45de0188587fc0b5c321
SithumRaigamage <sraig2002@gmail.com>
2024-02-04 11:23:25 +0530

    changes to colour
    made changes to colors of buttons and text field

--- Calculator/src/CalculatorFrame.java
+++ Calculator/src/CalculatorFrame.java
@@ -7,7 +7,7 @@ public class CalculatorFrame extends JFrame {
         // Panel p1 for the numbers
         JPanel p1 = new JPanel();
         p1.setLayout(new GridLayout(4, 3, 10, 10));
-        p1.setBackground(Color.blue);
+        p1.setBackground(Color.gray);
 
         for (int i =1; i <= 9; i ++){
             JButton button = new JButton (Integer.toString(i));
@@ -30,21 +30,22 @@ public class CalculatorFrame extends JFrame {
         p1.add (dotButton);
 
         // set color for "zero" and "."
-        zeroButton.setBackground(Color.RED);
+        zeroButton.setBackground(Color.white);
+        zeroButton.setForeground(Color.black);
         zeroButton.setOpaque(true);
         zeroButton.setBorderPainted(false);
-        zeroButton.setForeground(Color.blue);
         zeroButton.setFont(new Font("Arial", Font.BOLD, 20));
-        dotButton.setBackground(Color.RED);
+
+        dotButton.setBackground(Color.white);
+        dotButton.setForeground(Color.black);
         dotButton.setOpaque(true);
         dotButton.setBorderPainted(false);
-        dotButton.setForeground(Color.blue);
         dotButton.setFont(new Font("Arial", Font.BOLD, 20));
 
 
         //panel p2 for the operations
         JPanel p2 = new JPanel();
-        p2.setBackground(Color.blue);
+        p2.setBackground(Color.white);
         p2.setLayout(new GridLayout(4, 1, 2, 2));
 
         JButton plusButton = new JButton("+");
@@ -53,23 +54,33 @@ public class CalculatorFrame extends JFrame {
         p2.add(minusButton);
         JButton clearButton = new JButton("Clear");
         p2.add(clearButton);
+        JButton equalsButton=new JButton("=");
+        p2.add(equalsButton);
 
         // set color for "+", "-" and "Clear"
-        plusButton.setBackground(Color.green);
+        plusButton.setBackground(Color.orange);
         plusButton.setOpaque(true);
         plusButton.setBorderPainted(false);
-        plusButton.setForeground(Color.blue);
+        plusButton.setForeground(Color.black);
         plusButton.setFont(new Font("Arial", Font.BOLD, 20));
-        minusButton.setBackground(Color.green);
+
+        minusButton.setBackground(Color.orange);
         minusButton.setOpaque(true);
         minusButton.setBorderPainted(false);
-        minusButton.setForeground(Color.blue);
+        minusButton.setForeground(Color.black);
         minusButton.setFont(new Font("Arial", Font.BOLD, 20));
-        clearButton.setBackground(Color.green);
+
+        clearButton.setBackground(Color.orange);
         clearButton.setOpaque(true);
         clearButton.setBorderPainted(false);
-        clearButton.setForeground(Color.blue);
+        clearButton.setForeground(Color.black);
         clearButton.setFont(new Font("Arial", Font.BOLD, 20));
+
+        equalsButton.setBackground(Color.orange);
+        equalsButton.setOpaque(true);
+        equalsButton.setBorderPainted(false);
+        equalsButton.setForeground(Color.black);
+        equalsButton.setFont(new Font("Arial", Font.BOLD, 20));
 
         //panel p3 that combines p1 and p2
         JPanel p3 = new JPanel();
@@ -79,7 +90,7 @@ public class CalculatorFrame extends JFrame {
 
         // in the content frame (Which has by default a Border layout) we combined p3 and the textfiled
         JTextField txtField = new JTextField();
-        txtField.setBackground(Color.yellow);
+        txtField.setBackground(Color.white);
         //clearButton.setOpaque(true);
         //clearButton.setBorderPainted(false);
         txtField.setForeground(Color.blue);
